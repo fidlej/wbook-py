7f24ebc028c09cfec80195e054e5818d66ff6fc7
ivo <ivo@localhost>
2009-08-08 18:07:49 +0200

    Used a binary search in the dict.

--- libwbook/outputing.py
+++ libwbook/outputing.py
@@ -1,11 +1,13 @@
 
+import sys
+
 class Outputer(object):
     def __init__(self, options):
         pass
 
     def display(self, results):
-        for line in results:
-            print line
+        for row in results:
+            sys.stdout.write(row)
 
     def say(self, line):
         #TODO: start the festival
--- libwbook/searching.py
+++ libwbook/searching.py
@@ -1,13 +1,34 @@
 
+import bisect
+import os.path
+
 class Search(object):
     def __init__(self, options):
-        pass
+        self.back = options.back
+        self.forth = options.forth
+        self.dictionary = _FileAsList(options.dict)
 
     def find(self, line):
-        results = []
-        #TODO: do real search
-        for i in range(15):
-            results.append("%s: %s" % (line, i))
+        index = bisect.bisect_left(self.dictionary, line)
+        start = max(0, index - self.back)
+        end = index + self.forth
+        results = [self.dictionary[i] for i in xrange(start, end)]
+        return self.dictionary[start:end]
 
-        return results
+class _FileAsList(object):
+    def __init__(self, filename, line_width=82):
+        self.file = open(filename)
+        self.line_width = line_width
+        self.len = os.path.getsize(filename)//line_width
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            indices = index.indices(self.len)
+            return [self[i] for i in xrange(*indices)]
+        else:
+            self.file.seek(self.line_width * index)
+            return self.file.read(self.line_width)
+
+    def __len__(self):
+        return self.len
 
